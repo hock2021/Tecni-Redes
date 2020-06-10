36a9218366d24b73d721f952de87940a11986899
Tecni-Redes <tecni-redes@hotmail.com>
2020-06-10 18:40:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Tecni-Redes</title>
+
+<link href="css/simpleGridTemplate.css" rel="stylesheet" type="text/css">
+	
+<style type="text/css">
+a:link {
+    text-decoration: none;
+}
+a:visited {
+    text-decoration: none;
+}
+a:hover {
+    text-decoration: none;
+}
+a:active {
+    text-decoration: none;
+    text-align: left;
+}
+</style>
+	<link rel="stylesheet" href="css/whatsapp.css">
+	<link rel="stylesheet" href="css/fonts.css">
+	
+</head>
+<body>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://api.whatsapp.com/send?phone=+593996244676&text=Hola! Bienvenidos! a Tecni-Redes.." class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+<!-- Main Container -->
+<div class="container"> 
+  <!-- Header -->
+  <header class="copyright">
+	  
+	            <h2 class="my-float"> <img src="red1.png" alt="" width="225" height="100" class="header" align="middle">&nbsp; &nbsp; &nbsp; &nbsp; <a href="pagina web redes.html"><font color="white">&emsp;&emsp;<span class="icon-home"></span>&ensp;Inicio  </a> &nbsp; <a href="Servicios.html"><font color="white">&emsp;&emsp;<span class="icon-briefcase"></span>&ensp;Servicios</a> <a href="Contactos.html"><font color="white"> &nbsp; &emsp;&emsp;<span class="icon-address-book"></span>&ensp;Contactos </a><a <a href="Nosotros.html"><font color="white">&emsp;&emsp;<span class="icon-users"></span>&ensp;Nosotros</a>&nbsp; </h2>
+  </header>
+  <!-- Hero Section -->
+  <section class="intro">
+    <div class="column">
+	  <h3><font color="red">TECNI-REDES </font></h3><center><img src="tecniredes.JPG" alt="" width="400" height="250" class="profile"> <center></div> 
+	  <div class="column"><h3><font color="red">&nbsp; &nbsp; &nbsp; &nbsp; SERVICIO GARANTIZADO Y PROFESIONAL </font></h3> <p>Contamos con diferentes servicios como Mantenimiento Y Reparacion de 
+		  Computadoras/Laptop, Servicios de redes a domicilio, asistencia en proyectos de tesis y proyectos universitarios</p>
+	<center><img src="1.JPG" alt="" width="650" height="100" class="profile" align="middle"> </center></div>
+		<p>
+			
+    <p class="hero_header">SOLUCIONES INMEDIATAS</p>
+	               <p class="hero_header"><font color="Red">TELEF: <font color="white">099046469 - 0996244676</p>
+		
+    </div>
+  </section>
+  <!-- Stats Gallery Section -->
+  <div class="gallery">
+    <div class="thumbnail"> <a href="#"><img src="images/pc1.JPG" alt="" width="569" class="cards"/></a>
+      <h4>REPARACION DE COMPUTADORAS</h4>
+      <p class="tag">Mantenimiento,Reparacion discos duros, Reparacion placa madre, Reparacion General</p>
+      <p class="text_column">Brindamos servicios de mantenimientos preventivos, correcciones y respaldos de discos duros, Reparacion de placas madres y fuentes de poder, instalaciones de programas antivirus y sistemas operativos.</p>
+    </div>
+    <div class="thumbnail"> <a href="#"><img src="z1.jpg" alt="" width="569" class="cards"/></a>
+      <h4>&emsp;&emsp;SOPORTE DE REDES</h4>
+      <p class="tag">Instalacion/mantenimiento preventivo y correctivo de puntos y cableado de red de su negocio, empresa u hogar </p>
+      <p class="text_column">Brindamos servicio de instalaciones en servidores como windows server ubuntu centos ademas de instalaciones de cableado estructurado, reparacion de puntos de red y configuraciones en equipos.
+    </div>
+    <div class="thumbnail"> <a href="#"><img src="images/tesis.jpg" alt="" width="569" class="cards"/></a>
+      <h4>TESIS - PROYECTOS UNIVERSITARIOS</h4>
+      <p class="tag">Desarrollo de Tesis y proyectos</p>
+      <p class="text_column">Brindamos Asistencias en el desarrollo y seguimiento de tesis, ademas de proyectos en diferentes materias universitarias Instalaciones, simulaciones de servidores en maquinas virtuales puede comunicarse y detallar el desarrollo de su proyecto.</p>
+    </div>
+    <div class="thumbnail"></a>
+      <h4>MISION Y VISION</h4>
+	  <font color="Red">
+		  <p class="tag">MISION</p></font>
+      <p class="text_column"> Ofrecer sistemas de capacitación e información a los clientes con calidad, amabilidad y puntualidad, permitiendo en asistir a cumplir con los objetivos solicitados de cada cliente.</p>
+	  <p class="tag">VISION</p>
+	  <p class="text_column">La visión de la Empresa es ser reconocida a nivel nacional en el área de la Informática, Además, evolucionar en nuevos negocios que estén vinculados a las necesidades de servicios de INTERNET, equipos informáticos, , promoviendo servicios que mejoran la vida de nuestros clientes con calidad</p>
+    </div>
+    
+
+<iframe src="https://www.google.com/maps/d/embed?mid=1Us5n4CQrge2xIp8Eaj-Yl71ozdTUluQy&hl=es" width="1340" height="481"></iframe>
+		<br>
+  <footer class="header" id="contact">
+    <p class="hero_header">CONTACTANOS</p>
+    <div class="button">tecni-redes@hotmail.com </div>
+    <p> <div class="fb-share-button" data-href="https://tecni-redes.com" data-layout="button_count" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A//tecni-redes.com/&src=sdkpreparse" class="fb-xfbml-parse-ignore"><br><center><font color="red">PULSA AQUI <br>
+	<font color="white">COMPARTENOS EN FACEBOOK<br><font color="blue"><span class="icon-facebook2"></span></center></a></div>
+  </footer>
+  <!-- Copyrights Section -->
+<div class="copyright">&copy;WhatsApp - <strong>099046469 - 0996244676</strong></div>
+</div>
+<!-- Main Container Ends -->
+	  </section>
+</body>
+</html>
